60590c61cd552e506ac9d596014e9acd11e5bb21
NicolaL95 <nicola.lilla@gmail.com>
2021-12-09 17:21:07 +0100

    Fine lavorazione base

--- layouts/main.php
+++ layouts/main.php
@@ -1,14 +1,14 @@
   <div id="app">
  <main id='main_container'>
      <div class="container">
-         <div class="row">  
-     <div class="song_container"  v-for='song in songs'>
-               <img :src="song.poster" alt="" />
-               <h3>{{song.title}}</h3>
-            <p>
+         <div class="row justify-content-center row_song">  
+     <div class="song_container d-flex flex-column align-items-center text-center"  v-for='song in songs'>
+               <img :src="song.poster" alt="Song Image" />
+               <h3>{{song.title.toUpperCase()}}</h3>
+            <p class='p_author'>
               {{song.author}}
             </p>
-            <p>
+            <p class='p_year'>
               {{song.year}}
             </p>
            </div>
