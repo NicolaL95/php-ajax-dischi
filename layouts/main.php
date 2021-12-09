8b6b8375da71ad6331026909caf702683babb734
NicolaL95 <nicola.lilla@gmail.com>
2021-12-09 15:59:51 +0100

    Add structure on board

--- layouts/main.php
+++ layouts/main.php
@@ -1,6 +1,18 @@
- <main>
-        <div id="app">
-            
-        </div>
-        
+  <div id="app">
+ <main id='main_container'>
+     <div class="container">
+         <div class="row">  
+     <div class="song_container"  v-for='song in songs'>
+               <img :src="song.poster" alt="" />
+               <h3>{{song.title}}</h3>
+            <p>
+              {{song.author}}
+            </p>
+            <p>
+              {{song.year}}
+            </p>
+           </div>
+           </div>
+     </div>    
     </main>
+          </div>
