be020cd326a6ad1a5fe40b308867274e69407c69
NicolaL95 <nicola.lilla@gmail.com>
2021-12-10 16:16:33 +0100

    Added milestone2

--- milestone2/assets/app.js
+++ milestone2/assets/app.js
@@ -1,7 +1,9 @@
 const app = new Vue({
     el: '#app',
     data: {
-        songs: null
+        songs: null,
+        genreSongs: [],
+        selGen: 'all',
     },
     mounted() {
         axios
@@ -9,8 +11,28 @@ const app = new Vue({
             .then(response => {
                 console.log(response);
                 this.songs = response.data
+                this.songs.forEach(element => {
+                    if (!this.genreSongs.includes(element.genre)) {
+                        this.genreSongs.push(element.genre)
+                    }
+                });
             }).catch(error => {
                 console.log(error);
             })
+    },
+    methods: {
+        filterGenre() {
+            axios
+                .get('./songs.php')
+                .then(response => {
+                    this.songs = response.data
+                    if (this.selGen != 'all') {
+                        this.songs = this.songs.filter(function (item) {
+                            console.log(app.selGen)
+                            return item.genre == app.selGen
+                        });
+                    }
+                })
+        }
     }
 })
